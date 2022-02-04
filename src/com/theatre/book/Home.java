37ae1a3371082a6edb1a491f2e59b878991ba3f1
pravinnirmal <pravin.nirmal.doss@gmail.com>
2022-02-04 11:06:24 +0530

    Added seperate seats available status in homepage

--- src/com/theatre/book/Home.java
+++ src/com/theatre/book/Home.java
@@ -1,13 +1,10 @@
 package com.theatre.book;
 
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Time;
 import java.util.Scanner;
-
-
 
 public class Home {
 	static int available;
@@ -16,45 +13,55 @@ public class Home {
 	static int snumber;
 
 	public static void main(String[] args) throws SQLException {
-		Scanner scan = new Scanner (System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Counter Booking (1)  | Admin Access (2)");
 		int enter = scan.nextInt();
-		
-		if (enter ==1) {
+
+		if (enter == 1) {
 			System.out.println("Welcome to cinemas booking");
 			homePage();
-			}
-		else if(enter == 2) {
+		} else if (enter == 2) {
 			AdminPanel.admin();
-		}
-		else {
+		} else {
 			System.out.println("Enter correct option. Try Again..");
 			Home.main(args);
 			scan.close();
 		}
 	}
-	
+
 	public static void homePage() throws SQLException {
-		String query = "SELECT * FROM screen1";
-		
+
 		System.out.println("---------------------------");
 		ConnectionPage s = new ConnectionPage();
 		s.connect();
-		Statement stt = s.conn.createStatement();
-		ResultSet rs = stt.executeQuery(query);
 
-		while (rs.next()) {
-			snumber = rs.getInt(1);
-			movieRun = rs.getString(2);
-			available = rs.getInt(4);
-			screentime = rs.getTime(3);
+		Statement stt = s.conn.createStatement();
+
+		for (int i = 1; i < 5; i++) {
+			String aQuery = "SELECT COUNT(S" + i + ") FROM `seat_status-s1` WHERE S" + i + " ='Available'";
+			String query = "SELECT * FROM screen1 WHERE `Show Number` = " + i;
+			ResultSet rs2 = stt.executeQuery(aQuery);
+
+			while (rs2.next()) {
+				available = rs2.getInt(1);
+			}
+			ResultSet rs = stt.executeQuery(query);
+			while (rs.next()) {
+				snumber = rs.getInt(1);
+				movieRun = rs.getString(2);
+				screentime = rs.getTime(3);
+
+			}
 			System.out.println("Show Number     : " + " " + snumber);
 			System.out.println("Movie           : " + movieRun);
 			System.out.println("Show Timings    : " + screentime);
 			System.out.println("Available Seats : " + available);
 			System.out.println("-----------------------------");
+
 		}
 
 		TicketBooking.book();
+
 	}
+
 }
